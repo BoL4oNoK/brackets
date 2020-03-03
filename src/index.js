f846157078b1d91254aaf9374aea70508fc1edf2
Maksim Antonov <33826709+BoL4oNoK@users.noreply.github.com>
2020-03-03 12:37:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) return false;
+    
+    let pair = [];
+    bracketsConfig.forEach(elem => {
+        elem = elem[0] + "" + elem[1];
+        pair.push(elem.replace(/\(|\)|\[|\]|\||\{|\}/g, "\\$&"));
+    });
+
+    pair = pair.join('|');
+    let reg = new RegExp(pair, 'g');
+    let subStr;
+    while (subStr != str) {
+        subStr = str;
+        str = str.replace(reg, '');
+    }
+
+    return !str;
 }
